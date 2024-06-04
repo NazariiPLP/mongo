0ecb446202c814a6603f9cf860f2257ec5eab442
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-06-04 20:48:35 +0300

    Initialize server, add Salad module

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/');
+app.post('/');
+app.get('/:saladId');
+app.put('/:saladId');
+app.delete('/:saladId');
+
+
+module.exports = app;
